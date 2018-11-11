88907d514cc3e850b976aafaaa520ab804ce53bf
Priscilla Vazquez <scillamae922@gmail.com>
2018-11-11 21:02:35 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+speaker_names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+speaker_names.each do |speaker_name|
+  puts "Hello, my name is #{speaker_name}."
+  
+end
+
+room_numbers = [1,2,3,4,5,6,7]
+
+room_numbers.each do |room_number|
+  puts "You are assigned to #{room_number}."
+end
+
+batch_badge_creator = [#{speaker_name} + #{room_number}]
+ 
+  
+batch_badge_creator.each do |batch_badge_creator|
+  puts "#{speaker_name} + #{room_number}"
+  end
+end
