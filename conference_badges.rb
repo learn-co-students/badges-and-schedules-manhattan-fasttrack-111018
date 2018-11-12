cec0064d8f13299029a16004330af5a0676a4448
Nia B <nia_b@live.com>
2018-11-12 19:09:17 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,10 +21,10 @@ return greet
 
 
 def printer(attendees)
-  batch_badge_creator(attendees).each do |x|
-    puts x
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
   end
-  assign_rooms(attendees).each do |x|
-    puts x
+  assign_rooms(attendees).each do |assignment|
+    puts assignment
   end
 end
