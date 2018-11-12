941cf3c431c5cb052c31d4be87637cb8859fcd93
Ciprian Popa <ciprian_popa2002@yahoo.com>
2018-11-12 17:40:14 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def batch_badge_creator(name)
+  puts "Hello, my name is, #{name}"
+  array_of_names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  name .each do |name|
+    greet(name)
+end
+  
+  def assign_rooms(array_of_names)
+    rooms = [1, 2, 3, 4, 5, 6, 7]
+    rooms .each do |rooms|
+      puts rooms + name
+        puts "Hello, #{name}! You'll be assigned to room #{rooms}!" 
+  end
